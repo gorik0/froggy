0136f6184244529f47d5ee13b54fb3461e6465c8
gorik0 <delavarus@gmail.com>
2024-10-10 22:01:34 +0300

    add_replace_strategy

--- froggy.go
+++ froggy.go
@@ -2,6 +2,9 @@ package froggy
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"runtime/debug"
 	"sync"
 	"time"
 )
@@ -9,6 +12,24 @@ import (
 type ResizingStrategy interface {
 	Resize(runningWorkers, minWorkers, maxWorkers int) bool
 }
+
+const (
+	// defaultIdleTimeout defines the default idle timeout to use when not explicitly specified
+	// via the IdleTimeout() option
+	defaultIdleTimeout = 5 * time.Second
+)
+
+var (
+	// ErrSubmitOnStoppedPool is thrown when attempting to submit a task to a pool that has been stopped
+	ErrSubmitOnStoppedPool = errors.New("worker pool has been stopped and is no longer accepting tasks")
+)
+
+// defaultPanicHandler is the default panic handler
+func defaultPanicHandler(panic interface{}) {
+	fmt.Printf("Worker exits from a panic: %v\nStack trace: %s\n", panic, string(debug.Stack()))
+}
+
+type Option func(pool *WorkerPool)
 
 type WorkerPool struct {
 	//	atomic values
@@ -34,4 +55,35 @@ type WorkerPool struct {
 	tasksWaitGroup   sync.WaitGroup
 	mutex            sync.Mutex
 	stopped          int32
+}
+
+func NewPool(maxWorkers int, maxCapacity int, options ...Option) *WorkerPool {
+
+	pool := &WorkerPool{
+		maxWorkers:   maxWorkers,
+		maxCapacity:  maxCapacity,
+		idleTimeout:  defaultIdleTimeout,
+		strategy:     Eager(),
+		panicHandler: defaultPanicHandler,
+	}
+
+	// Apply all options
+	for _, opt := range options {
+		opt(pool)
+	}
+
+	// Make sure options are consistent
+	if pool.maxWorkers <= 0 {
+		pool.maxWorkers = 1
+	}
+	if pool.minWorkers > pool.maxWorkers {
+		pool.minWorkers = pool.maxWorkers
+	}
+	if pool.maxCapacity < 0 {
+		pool.maxCapacity = 0
+	}
+	if pool.idleTimeout < 0 {
+		pool.idleTimeout = defaultIdleTimeout
+	}
+	return
 }
